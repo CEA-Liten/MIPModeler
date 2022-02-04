f7e34808cdc0fe5755aaccfb42990129b4af95aa
Bernard SécherÃ <bernard.secher@cea.fr>
2022-02-04 11:51:50 +0100

    test FbsfFramework

--- environ.sh
+++ environ.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 export TRILOGY_ROOT=/volatile/Trilogy
 export FBSF_HOME=${TRILOGY_ROOT}/fbsfOpenSource
-if [ -d ${FBSF_HOME}/lib/release ] 
+if [ -f ${FBSF_HOME}/lib/release/FbsfFramework ]
 then
   export FBSF_BUILD_MODE=release
-elif [ -d ${FBSF_HOME}/lib/debug ] 
+elif [ -f ${FBSF_HOME}/lib/debug/FbsfFramework ]
 then
   export FBSF_BUILD_MODE=debug
 else
-  echo "Fbsf libraries not found!"
+  echo "Fbsf not found!"
   exit
 fi
+echo "mode "${FBSF_BUILD_MODE}
 source ${FBSF_HOME}/QtVersion.sh
 export CPLEX_HOME=${TRILOGY_ROOT}/cplex/cplex
 export MIPMODELER_INSTALL=${TRILOGY_ROOT}/MIPModeler_install
