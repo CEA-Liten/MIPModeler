a0dc56663a94f006ed0fcfc3fcd339452b3185be
AR170699 <alain.ruby@cea.fr>
2022-02-14 13:40:27 +0100

    Fix path with space blanks on servers

--- MIPSolverInterface/CplexEnv.bat
+++ MIPSolverInterface/CplexEnv.bat
@@ -3,9 +3,9 @@ set CPLEX_HOME_BIN=
 echo "in CPLEX_ENV SOLVER_DEPS=%SOLVER_DEPS%"
 rem if installed on Program files
 if exist "C:\Program Files\IBM\ILOG\CPLEX_Studio201\cplex\bin\x64_win64\cplex2010.dll" (
-	set CPLEX_HOME="C:\Program Files\IBM\ILOG\CPLEX_Studio201\cplex"
-	set CPLEX_HOME_BIN="C:\Program Files\IBM\ILOG\CPLEX_Studio201\cplex\bin\x64_win64"
-	set CPLEX_HOME_LIB="C:\Program Files\IBM\ILOG\CPLEX_Studio201\cplex\lib\x64_windows_vs2015\stat_mda"
+	set CPLEX_HOME=C:\Program Files\IBM\ILOG\CPLEX_Studio201\cplex
+	set CPLEX_HOME_BIN=C:\Program Files\IBM\ILOG\CPLEX_Studio201\cplex\bin\x64_win64
+	set CPLEX_HOME_LIB=C:\Program Files\IBM\ILOG\CPLEX_Studio201\cplex\lib\x64_windows_msvc14\stat_mda
 	goto :END
 )
 rem if Cplex 20.10
@@ -17,9 +17,9 @@ if exist %SOLVER_DEPS%\Cplex\CPLEX_Studio201\cplex\bin\x64_win64\cplex2010.dll (
 )
 rem if installed on Program files
 if exist "C:\Program Files\IBM\ILOG\CPLEX_Studio129\cplex\bin\x64_win64\cplex1290.dll" (
-	set CPLEX_HOME="C:\Program Files\IBM\ILOG\CPLEX_Studio129\cplex"
-	set CPLEX_HOME_BIN="C:\Program Files\IBM\ILOG\CPLEX_Studio129\cplex\bin\x64_win64"
-	set CPLEX_HOME_LIB="C:\Program Files\IBM\ILOG\CPLEX_Studio129\cplex\lib\x64_windows_vs2015\stat_mda"
+	set CPLEX_HOME=C:\Program Files\IBM\ILOG\CPLEX_Studio129\cplex
+	set CPLEX_HOME_BIN=C:\Program Files\IBM\ILOG\CPLEX_Studio129\cplex\bin\x64_win64
+	set CPLEX_HOME_LIB=C:\Program Files\IBM\ILOG\CPLEX_Studio129\cplex\lib\x64_windows_vs2015\stat_mda
 	goto :END
 )
 rem if Cplex 12.9
