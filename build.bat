4f0830865995259a8d716167207450e3439e6dc8
AR170699 <alain.ruby@cea.fr>
2022-09-16 17:28:14 +0200

    change build of MipModeler to use log file + dbg print of constraint with negative col/variable index at model assembling state (case when a constraint is unsing undefined variable)

--- build.bat
+++ build.bat
@@ -2,6 +2,7 @@ REM for RELEASE set OPTION=release
 REM for debug set OPTION=debug
 REM OPTION must be defined by calling script
 REM Or OPTION is defined by user in interactive mode (detected by void option)
+setlocal enabledelayedexpansion
 
 if /i "%OPTION%" == "" (
 ECHO A build mode is required. please enter release or debug
@@ -21,16 +22,27 @@ REM call AppEnv.bat -> replaced by PegaseEnv
 REM clean
 if exist Makefile call jom distclean
 
+set log_file="%CUR_PATH%\compile.%OPTION%.log"
+echo "Compiling %BUILD_PATH% in mode %OPTION%" > %log_file%
+
 REM Create makefiles
 call qmake
 
 REM compile
 if %OPTION%==debug (
-call jom debug
+call jom debug >> %log_file%
+if not !ERRORLEVEL! == 0 goto sortie
+call jom.exe -f Makefile.Debug install
 )
 if %OPTION%==release (
-call jom
+call jom >> %log_file%
+if not !ERRORLEVEL! == 0 goto sortie
+call jom.exe -f Makefile.Release install
 )
 
 cd %CUR_PATH%
 set PATH=%OLD_PATH%
+
+:sortie
+ echo "FATAL COMPILATION OF %BUILD_PATH% in mode %OPTION%" 
+ EXIT /B 1
