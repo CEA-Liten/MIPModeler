d29c556c4d580a94d254fe8ccd1efe47a85e1b60
JC799343 <joel.chebassier@cea.fr>
2023-04-03 17:36:23 +0200

    Flag NoGams

--- GAMSModeler/GAMSEnv.bat
+++ GAMSModeler/GAMSEnv.bat
@@ -8,7 +8,7 @@ if exist C:\GAMS\39 set GAMS_HOME=C:\GAMS\39
 if %GAMS_HOME%=="" goto sortie	
 set USE_GAMS=%GAMS_HOME%
 set PATH=%GAMS_HOME%;%GAMS_HOME%\apifiles\C++\lib\vs2019;%PATH%;
-
+echo USE_GAMS=%GAMS_HOME%
 :sortie
 
 
--- MIPSolverInterface/MIPCpxSolver/CplexEnv.bat
+++ MIPSolverInterface/MIPCpxSolver/CplexEnv.bat
@@ -3,8 +3,8 @@ set CPLEX_HOME_LIB=
 set CPLEX_HOME_BIN=
 set CPLEX_VERSION=
 
-set CPLEX_STUDIO_DIR201=
-set CPLEX_STUDIO_DIR129=
+:: set CPLEX_STUDIO_DIR201=
+:: set CPLEX_STUDIO_DIR129=
 
 
 REM Find cplex installation using system environement variable
