cf445c07d8b15a4fed21ff8d86e70c88429fcf1f
JC799343 <joel.chebassier@cea.fr>
2025-06-23 14:26:09 +0200

    suppression de GAMSModeler et des .pro

--- GAMSModeler/GAMSData.cpp
+++
@@ -1,9 +0,0 @@
-#include "GAMSData.h"
-
-namespace GAMSModeler {
-
-GAMSData::GAMSData(const std::string& name, const GAMSDataType& type):mName(name), mType(type) {}
-
-GAMSData::~GAMSData() {}
-
-};
--- MIPSolverInterface/MIPCpxSolver/CplexEnv.sh
+++
@@ -1,4 +0,0 @@
-export CPLEX_HOME=${PEGASE_CPLEX}/cplex
-export CPLEX_HOME_BIN=${CPLEX_HOME}/bin/x86-64_linux
-export CPLEX_HOME_LIB=${CPLEX_HOME}/lib/x86-64_linux/static_pic
-export CPLEX_VERSION=2010
